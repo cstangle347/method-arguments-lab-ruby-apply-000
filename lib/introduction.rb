ea74d72cb85be9e73465a066267739453ac0371e
Christopher Stangle <christopherstangle@gmail.com>
2019-03-18 15:28:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,7 @@
 #introduction
 
-def introduction(name)
-  puts "Hi, my name is #{name}."
+#introduction_with_language
+
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
